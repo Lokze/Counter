8bb548f7d6b7adaf53f4140a476c16c0c1644cef
Lorenzo Loi <51636003+Lokze@users.noreply.github.com>
2022-02-08 18:24:23 +0100

    Finalization
    added text hover the counter, and correction of some path issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 
 function add(){
    let count=parseInt(document.getElementById("counter").innerHTML);
-   alert(count);
    count++;
-   alert(count);
    document.getElementById("counter").innerHTML=count;
 }
 
 function sub(){
     let count=parseInt(document.getElementById("counter").innerHTML);
-    alert(count);
     count--;
-    alert(count);
     document.getElementById("counter").innerHTML=count;
  }
  
