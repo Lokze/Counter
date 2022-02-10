f0c55ce1b1950bbc43df96c18ea7a494785f03db
Lorenzo Loi <51636003+Lokze@users.noreply.github.com>
2022-02-10 16:02:47 +0100

    correction of javaScript
    now p and the button are created via JS,
    and the count functions do not relay on the "onclick" atribute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,44 @@
+/* Creation of the <p> tag */
+const para = document.createElement("p");
+const pNode = document.createTextNode("0");
+para.appendChild(pNode);
+para.id="counter";
+const element = document.getElementById("container");
+element.appendChild(para);
 
-function add(){
+/* Creation of the buttons*/
+const divB = document.createElement("div");
+const btAdd = document.createElement("button");
+const btSub = document.createElement("button");
+
+divB.appendChild(btSub);
+divB.appendChild(btAdd);
+element.appendChild(divB);
+divB.className="buttons";
+
+btAdd.className="button";
+btAdd.type="button";
+btAdd.innerText="+1";
+
+btSub.className="button";
+btSub.type="button";
+btSub.innerText="-1";
+
+/*Count functions*/
+btAdd.addEventListener("click",function(){
    let count=parseInt(document.getElementById("counter").innerHTML);
    count++;
    document.getElementById("counter").innerHTML=count;
-}
+})
 
-function sub(){
-    let count=parseInt(document.getElementById("counter").innerHTML);
+btSub.addEventListener("click",function(){
+   let count=parseInt(document.getElementById("counter").innerHTML);
     count--;
     document.getElementById("counter").innerHTML=count;
- }
- 
+})
+
+
+
+
+
+
